5f2ecb9fdfafeacc23b540ae8097c7a108706b92
Adam Setch <adam.setch@outlook.com>
2026-08-27 09:30:13 -0700

    fix: gtag
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/analytics.js
+++ src/analytics.js
@@ -1,19 +1,14 @@
 if (process.env.GTM_ID) {
-  ;(function (w, d, s, i) {
+  ;(function (w, d, s, l, i) {
+    w[l] = w[l] || []
+    w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' })
     var f = d.getElementsByTagName(s)[0]
     var j = d.createElement(s)
+    var dl = l !== 'dataLayer' ? '&l=' + l : ''
     j.async = true
-    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i
+    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl
     f.parentNode.insertBefore(j, f)
-
-    w.dataLayer = w.dataLayer || []
-    function gtag() {
-      w.dataLayer.push(arguments)
-    }
-    gtag('js', new Date())
-
-    gtag('config', i)
-  })(window, document, 'script', process.env.GTM_ID)
+  })(window, document, 'script', 'dataLayer', process.env.GTM_ID)
 }
 if (process.env.ADOBE_LAUNCH_SCRIPT_URL) {
   ;(function (w, d, s, i) {
